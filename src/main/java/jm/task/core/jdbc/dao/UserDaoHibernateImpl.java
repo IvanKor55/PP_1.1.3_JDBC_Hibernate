bc152f22a297f923b4dd0ada087b5b0466c2d3b1
Ivan Korobov <ivankor48@gmail.com>
2023-12-21 11:39:27 +0300

    Работа с базой данных c помощью Hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -8,8 +8,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
 import javax.persistence.Query;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -22,54 +20,43 @@ public class UserDaoHibernateImpl implements UserDao {
                                      age TINYINT);
                               """;
     private static final String DROPUSERSTABLE = "DROP TABLE IF EXISTS user;";
-    private static final String CLEANUSERSTABLE = "DELETE FROM user;";
+    private static final String CLEANUSERSTABLE = "DELETE from User;";
+    private static final String GETALLUSERS = "SELECT u FROM User u";
     public UserDaoHibernateImpl() {
     }
 
 
     @Override
     public void createUsersTable() {
-        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
+            Transaction transaction = session.beginTransaction();
             Query query = session.createSQLQuery(CREATEUSERSTABLE).addEntity(User.class);
             query.executeUpdate();
             transaction.commit();
         } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public void dropUsersTable() {
-        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
+            Transaction transaction = session.beginTransaction();
             Query query = session.createSQLQuery(DROPUSERSTABLE).addEntity(User.class);
             query.executeUpdate();
             transaction.commit();
         } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
+            Transaction transaction = session.beginTransaction();
             session.save(new User(name, lastName, age));
             transaction.commit();
         } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
             throw new RuntimeException(e);
         }
     }
@@ -95,10 +82,7 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public List<User> getAllUsers() {
         try (Session session = sessionFactory.openSession()) {
-            CriteriaQuery createQuery = session.getCriteriaBuilder().createQuery(User.class);
-            Root<User> root = createQuery.from(User.class);
-            Query query = session.createQuery(createQuery);
-            return query.getResultList();
+            return session.createQuery(GETALLUSERS, User.class).getResultList();
         } catch (HibernateException e) {
             throw new RuntimeException(e);
         }
@@ -106,16 +90,12 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
+            Transaction transaction = session.beginTransaction();
             Query query = session.createSQLQuery(CLEANUSERSTABLE).addEntity(User.class);
             query.executeUpdate();
             transaction.commit();
         } catch (HibernateException e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
             throw new RuntimeException(e);
         }
     }
